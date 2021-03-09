af5f8c91b7f79b4e6f96e0d5c75e588608d98f8b
lukastanojcic <66432620+lukastanojcic@users.noreply.github.com>
2021-03-09 13:59:12 +0100

    Add files via upload
    initial commit

---
+++ mountain-climbing/src/main/java/com/example/mountainclimbing/MountainClimbingApplication.java
@@ -0,0 +1,13 @@
+package com.example.mountainclimbing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainClimbingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountainClimbingApplication.class, args);
+	}
+
+}
---
+++ mountain-climbing/src/test/java/com/example/mountainclimbing/MountainClimbingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mountainclimbing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainClimbingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
