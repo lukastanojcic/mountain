202384e015e2aedacb42d1e5ebc382f63b10a1b4
lukastanojcic <lukastanojcic@gmail.com>
2021-03-11 11:21:30 +0100

    lombok getters and setters

--- mountain-climbing/src/main/java/com/example/mountainclimbing/config/DbCustomConfiguration.java
+++ mountain-climbing/src/main/java/com/example/mountainclimbing/config/DbCustomConfiguration.java
@@ -2,6 +2,8 @@ package com.example.mountainclimbing.config;
 
 import com.example.mountainclimbing.MountainClimbingApplication;
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -33,6 +35,7 @@ public class DbCustomConfiguration {
 	}
 
 	@ConfigurationProperties(prefix="mysql")
+	@Getter @Setter
 	public static class ConfigProperties {
 
 		private String driverClassName;
@@ -47,25 +50,6 @@ public class DbCustomConfiguration {
 			this.driverClassName=driverClassName;
 		}
 
-		public String getPassword() {
-			return password;
-		}
-
-		public void setPassword(String password) {
-			this.password=password;
-		}
-		public String getUsername() {
-			return username;
-		}
-		public void setUsername(String username) {
-			this.username=username;
-		}
-		public String getUrl() {
-			return url;
-		}
-		public void setUrl(String url) {
-			this.url=url;
-		}
 	}
 
 }
